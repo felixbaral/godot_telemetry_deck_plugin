4bff041b36612ad55c4d59a3b136ace47e037b78
felix baral <felixbaral@me.com>
2025-03-26 14:32:12 +0100

    addition of runtime and enriched data

--- TelemetryDeckAnalytics.gd
+++ TelemetryDeckAnalytics.gd
@@ -1,31 +1,39 @@
+class_name TelemetryDeck
 extends Node
 
 @onready var http: HTTPRequest = $HTTPRequest
 var url = "https://nom.telemetrydeck.com/v2/"
 @export var appId: String= "xxx"
+# https://telemetrydeck.com/docs/ingest/default-parameters/
+# https://telemetrydeck.com/docs/ingest/v2/
+var time_start_sec = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	time_start_sec = Time.get_ticks_msec() / 1000
 	sendSignal("application_start")
-	pass # Replace with function body.
 
+func _enricht_data(data: Dictionary) -> Dictionary:
+	var runtime =  ((Time.get_ticks_msec() / 1000) - time_start_sec)/60
+	data["runtime_m"] = runtime
+	return data
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func sendSignal(type: String):
+func sendSignalWithDict(type: String, dict: Dictionary):
 	print("sending "+type)
 	var headers = ["Content-Type: application/json; charset=utf-8"]
 	var data = [
 		{
 			"appID": appId,
 			"type": type,
-			"clientUser": "myClientUserHash"
+			"clientUser": "myClientUserHash",
+			"payload": _enricht_data(dict)
 		}
 	]
 	var json = JSON.stringify(data)
 	print(json)
 	http.request(url, headers, HTTPClient.METHOD_POST, json)
-	pass
+
+func sendSignal(type: String):
+	sendSignalWithDict(type, {})
+
+func sendSignalWithData(type: String, data: String):
+	sendSignalWithDict(type, {"data": data})
